8484d3173ddb54ad2e7f60d518ef481381c7415f
Void Ray <team.yudin@gmail.com>
2017-07-13 16:05:38 -0500

    switched to element's router config

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,11 +1,7 @@
 <template>
-  <el-menu theme="dark" :default-active="activeIndex" mode="horizontal" @select="handleSelect">
-    <el-menu-item index="1">
-      <router-link to="/" class="undecorated">Home</router-link>
-    </el-menu-item>
-    <el-menu-item index="2">
-      <router-link to="/about" class="undecorated">About</router-link>
-    </el-menu-item>
+  <el-menu theme="dark" :default-active="activeIndex" mode="horizontal" :router=true>
+    <el-menu-item index="/">Home</el-menu-item>
+    <el-menu-item index="about">About</el-menu-item>
   </el-menu>
 </template>
 
@@ -15,17 +11,10 @@
       return {
         activeIndex: '1'
       }
-    },
-    methods: {
-      handleSelect (key, keyPath) {
-        console.log(key, keyPath)
-      }
     }
   }
 </script>
 
 <style scoped>
-  .undecorated{
-    text-decoration: none
-  }
+
 </style>
